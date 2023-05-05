988e6acf8d6e434758c360c35081054127672580
Goba3 <37496766+Goba3@users.noreply.github.com>
2023-05-05 17:45:45 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let img = document.querySelector(".img");
+let container= document.querySelector(".container");
+function cars (cars){
+    img.src =cars;
+
+}
+function colors(color) {
+
+container. style.background= color ;
+
+}
